44470272c1874bf4d635cca21b79b603d60ceb32
hani <hani.fares93@gmail.com>
2020-04-14 11:40:09 +0200

    fix the Footer

--- corona_map_vue/src/App.vue
+++ corona_map_vue/src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="app" class="container-fluid">
+  <div id="app" class="container-fluid main-container">
     <nav-bar></nav-bar>
     <router-view />
-    <div class="card-footer text-muted">
-      Developed by: Hani Fares
-    </div>
+    <main-footer></main-footer>
   </div>
 </template>
 
 <script>
-  import NavBar from "./components/NavBar";
-  export default {
-    components: {
-      NavBar
-    }
+import NavBar from "./components/NavBar";
+import MainFooter from "./components/MainFooter";
+export default {
+  components: {
+    NavBar,
+    MainFooter
   }
+};
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  position: relative;
+}
+* {
+  box-sizing: border-box;
+}
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+.main-container {
+  min-height: 100vh; /* will cover the 100% of viewport */
+  overflow: hidden;
+  display: block;
+  position: relative;
+  padding-bottom: 100px; /* height of your footer */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ corona_map_vue/src/components/MainFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <footer>
+      <div class="alert alert-danger align-content-center" role="alert">
+        For you and your Family <strong>Please #StayHome</strong>
+      </div>
+      <div class="card-footer text-right text-muted">
+        Developed by: Hani Fares
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainFooter"
+};
+</script>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
